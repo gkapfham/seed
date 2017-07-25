5d8e7e00578b27b55d2f4280bd1a890253f89b65
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-07-25 19:00:49 -0400

    The starting index should be zero in the seed_process_create_internal_dictionary in the seed_process file.

--- seed_process.py
+++ seed_process.py
@@ -2,7 +2,7 @@
 
 
 DATA_PAYLOAD = "data"
-UPDATES = "Updates"
+UPDATES = "Updates from"
 SUBJECT = "_subject"
 EMAIL = "reply_to"
 
@@ -19,7 +19,13 @@ def seed_process_remove_email_subscriptions(seed_internal_dictionary_list):
     """ Process the JSON file by removing the subscription entries """
     processed_seed_list = seed_internal_dictionary_list
     for internal_dictionary in seed_internal_dictionary_list:
+        print('-----')
+        print(internal_dictionary)
+        print('-----')
+        print()
         if UPDATES in internal_dictionary[SUBJECT]:
+            print(UPDATES)
+            print(internal_dictionary)
             seed_internal_dictionary_index = seed_internal_dictionary_list.index(internal_dictionary)
             del processed_seed_list[seed_internal_dictionary_index]
 
@@ -28,11 +34,10 @@ def seed_process_create_internal_dictionary(seed_json):
     """ Process the JSON file and then create the internal dictionary """
     maximum_form_submission = len(seed_json)
     internal_dictionary_list = []
-    for current_form_submission_index in range(1, maximum_form_submission):
+    for current_form_submission_index in range(0, maximum_form_submission):
         current_form_submission = seed_json[current_form_submission_index]
         for key_specific_person, value_specific_person in current_form_submission.items():
             if key_specific_person == DATA_PAYLOAD:
                 submission_details_dict = value_specific_person
-                # print("SUBMISSION: ", submission_details_dict)
                 internal_dictionary_list.append(submission_details_dict)
     return internal_dictionary_list
