b1060a1169e4d0f5665c3590115c809a644e4006
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-07-25 14:29:37 -0400

    Adding the first version of the test_seed_download.py file.

---
+++ tests/test_seed_download.py
@@ -0,0 +1,31 @@
+"""Test suite for the seed module"""
+
+import os
+import pytest
+
+import seed
+import seed_download
+
+
+VERIFIED = True
+
+
+DOWNLOADED = True
+NOT_DOWNLOADED = False
+
+
+@pytest.fixture
+def verifiable_seed_args():
+    """Return arguments that are verifiable with the token specified through the OS"""
+    # test case can only pass if the environment variable is set
+    simple_form_token = os.environ.get('SIMPLEFORM_TOKEN')
+    return ['--token', simple_form_token, '--download-json']
+
+
+def test_seed_verified_downloaded_something(verifiable_seed_args):
+    """Run seed with a specified token and it is verified"""
+    seed_arguments, seed_parser = seed.parse_seed_arguments(verifiable_seed_args)
+    seed_args_verified = seed.verify_seed_arguments(seed_arguments)
+    assert seed_args_verified == VERIFIED
+    downloaded_json = seed_download.seed_download(seed_arguments.token)
+    assert downloaded_json is not None
