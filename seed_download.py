1afde1221cb44748e5cd2eccd6728d1f8813c50c
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-07-25 14:31:05 -0400

    Adding the first version of the seed_download.py file; it has a test case and it is, I think, correctly downloading the JSON file from SimpleForm.

---
+++ seed_download.py
@@ -0,0 +1,16 @@
+""" seed_download.py downloads a JSON file from SimpleForm """
+
+
+import requests
+
+
+def seed_download(seed_simpleform_token):
+    """ download the JSON file from SimpleForm using the provided token """
+    simpleform_response = requests.get('http://getsimpleform.com/messages.json?api_token=' + seed_simpleform_token)
+    response_json = None
+
+    # the query worked and the JSON file can be returned
+    if simpleform_response.ok:
+        # extract the JSON object
+        response_json = simpleform_response.json()
+    return response_json
