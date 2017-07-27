1690a9f0a9b5605693cec8d437cc8fc4844c1e42
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-07-27 12:27:27 -0400

    Removing some commented-out and debugging statements in the seed_process.py file.

--- seed_process.py
+++ seed_process.py
@@ -32,13 +32,11 @@ def seed_process_remove_email_subscriptions(seed_internal_dictionary_list):
 def seed_process_create_internal_dictionary(seed_json):
     """ Process the JSON file and then create the internal dictionary """
     maximum_form_submission = len(seed_json)
-    print("Maximum", maximum_form_submission)
     internal_dictionary_list = []
     for current_form_submission_index in range(0, maximum_form_submission):
         current_form_submission = seed_json[current_form_submission_index]
         for key_specific_person, value_specific_person in current_form_submission.items():
             if key_specific_person == DATA_PAYLOAD:
                 submission_details_dict = value_specific_person
-                # print("NEXT: ", submission_details_dict)
                 internal_dictionary_list.append(submission_details_dict)
     return internal_dictionary_list
