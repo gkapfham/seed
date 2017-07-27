1d487973a1fc3463ff714c608b3ec431ea2c1bdb
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-07-27 13:44:17 -0400

    Adding the first version of the seed_display file; it has two basic functions that seem to be working correctly.

---
+++ seed_display.py
@@ -0,0 +1,19 @@
+""" seed_display.py displays details about the SEED data """
+
+
+import json
+
+import seed_dictionary
+import seed
+
+def seed_display_respondents(seed_dictionary_list):
+    """ Displays all of the people who responded """
+    for current_seed_dictionary in seed_dictionary_list:
+        current_name = current_seed_dictionary[seed_dictionary.PERSON_NAME]
+        print(seed.INDENT, current_name)
+
+
+def seed_display_sample(seed_dictionary_list):
+    """ Displays all of the topics """
+    first_seed_dictionary = seed_dictionary_list[0]
+    print(json.dumps(first_seed_dictionary, indent=3))
