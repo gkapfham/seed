4dc55e7bcbd0fd2bef15af54767e2953c6dcfed9
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-07-27 16:41:14 -0400

    Adding the create_fact_answer_list function to the seed_create.py file.

--- seed_create.py
+++ seed_create.py
@@ -17,3 +17,12 @@ def create_mailing_list(seed_dictionary_list):
         current_list_entry = current_email + DIVIDER + QUOTATION_MARK + current_name + QUOTATION_MARK
         email_address_list.append(current_list_entry)
     return email_address_list
+
+
+def create_fact_answer_list(seed_dictionary_list):
+    """ Create the list of all of the answers to the FACT question """
+    fact_answers_list = []
+    for current_seed_dictionary in seed_dictionary_list:
+        current_fact = current_seed_dictionary[seed_lookup.QUESITON_FACT]
+        fact_answers_list.append(current_fact)
+    return fact_answers_list
