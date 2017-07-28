82b2cdf623bc58d27a2c0b9d2be3ec44c7aa7e74
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-07-28 11:37:40 -0400

    Fixing the docstring in a function in the seed_display file.

--- seed_display.py
+++ seed_display.py
@@ -1,11 +1,11 @@
 """ seed_display.py displays details about the SEED data """
 
-
 import json
 import random
 
 import seed_lookup
 import seed
+
 
 def seed_display_respondents(seed_dictionary_list):
     """ Displays all of the people who responded """
@@ -15,7 +15,7 @@ def seed_display_respondents(seed_dictionary_list):
 
 
 def seed_display_sample(seed_dictionary_list):
-    """ Displays all of the topics """
+    """ Displays all of the topics in a textual format """
     first_entry_index = 0
     last_entry_index = len(seed_dictionary_list)
     randomly_chosen_index = random.randint(first_entry_index, last_entry_index)
