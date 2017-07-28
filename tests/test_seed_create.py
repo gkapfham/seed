498ddaef44fe385260a9d2d453fc6644dfbf4b66
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-07-28 12:04:06 -0400

    Adding another test case to the test_seed_create file; this is a passing test case for the create_advice_answer_list function in seed_create.

--- tests/test_seed_create.py
+++ tests/test_seed_create.py
@@ -44,3 +44,17 @@ def test_fact_responses_for_each_name(verifiable_seed_args):
     assert len(seed_internal_dictionary_list) < length_internal_dictionary_list
     fact_answers_list = seed_create.create_fact_answer_list(seed_internal_dictionary_list)
     assert len(seed_internal_dictionary_list) == len(fact_answers_list)
+
+def test_advice_responses_for_each_name(verifiable_seed_args):
+    """Make sure that the removal of the email address makes a same-sized list"""
+    seed_arguments, seed_parser = seed.parse_seed_arguments(verifiable_seed_args)
+    downloaded_json = seed_download.seed_download(seed_arguments.token)
+    assert downloaded_json is not None
+    seed_internal_dictionary_list = seed_process.seed_process_create_internal_dictionary(downloaded_json)
+    assert seed_internal_dictionary_list is not None
+    length_internal_dictionary_list = len(seed_internal_dictionary_list)
+    assert length_internal_dictionary_list > 0
+    seed_process.seed_process_remove_email_subscriptions(seed_internal_dictionary_list)
+    assert len(seed_internal_dictionary_list) < length_internal_dictionary_list
+    advice_answers_list = seed_create.create_advice_answer_list(seed_internal_dictionary_list)
+    assert len(seed_internal_dictionary_list) == len(advice_answers_list)
