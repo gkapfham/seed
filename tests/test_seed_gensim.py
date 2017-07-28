b55d4e6213538b73c0689752e44fd9a500838ff1
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-07-28 12:42:18 -0400

    Grabbing both of the return values in the test_seed_gensim test case; this test case is again passing.

--- tests/test_seed_gensim.py
+++ tests/test_seed_gensim.py
@@ -12,6 +12,6 @@ def test_seed_gensim_creates_dictionary():
     """ Determine if seed_gensim can create a not-None dictionary """
     seed_dictionary_list = seed_download.seed_load()
     fact_response_list = seed_create.create_fact_answer_list(seed_dictionary_list)
-    topic_model_dictionary = seed_gensim.create_topic_model_dictionary(fact_response_list)
+    topic_model_dictionary, texts_to_analyze = seed_gensim.create_topic_model_dictionary(fact_response_list)
     assert topic_model_dictionary is not None
     assert len(topic_model_dictionary.keys()) != 0
