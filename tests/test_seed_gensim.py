a1c0946ee81d90c84efbd4819ae7d0f55d2a35b0
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-07-28 10:59:38 -0400

    Adding in the first test case to the test_seed_gensim file.

---
+++ tests/test_seed_gensim.py
@@ -0,0 +1,16 @@
+"""Test suite for the seed_gensim.py module"""
+
+import pytest
+
+import seed
+import seed_create
+import seed_download
+import seed_gensim
+
+
+def test_seed_gensim_creates_dictionary():
+    """ Determine if seed_gensim can create a not-None dictionary """
+    seed_dictionary_list = seed_download.seed_load()
+    fact_response_list = seed_create.create_fact_answer_list(seed_dictionary_list)
+    topic_model_dictionary = seed_gensim.create_topic_model_dictionary(fact_response_list)
+    assert topic_model_dictionary is not None
