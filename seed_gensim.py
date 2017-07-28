050fb5e2a917ff396a224ee08b719c97131c380e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-07-28 11:35:40 -0400

    This is a working draft of the seed_gensim file that contains more code for creating and analyzing the gensim model.

--- seed_gensim.py
+++ seed_gensim.py
@@ -8,16 +8,17 @@ import gensim
 
 
 def create_topic_model(list_responses):
+    """ Using LDA from gensim, create the topic model from the list of responses """
     topic_model_dictionary, texts_to_analyze = create_topic_model_dictionary(list_responses)
-    # convert tokenized documents into a document-term matrix
-    corpus = [topic_model_dictionary.doc2bow(text) for text in texts_to_analyze]
+    # convert tokenized documents into a document-term matrix, or the corpus
+    topic_model_corpus = [topic_model_dictionary.doc2bow(text) for text in texts_to_analyze]
     # generate LDA model from the texts_to_analyze and the topic_model_dictionary
-    lda_model = gensim.models.ldamodel.LdaModel(corpus, num_topics=3, id2word=topic_model_dictionary, passes=20)
-    return lda_model
+    lda_model = gensim.models.ldamodel.LdaModel(topic_model_corpus, num_topics=3, id2word=topic_model_dictionary, passes=20)
+    return lda_model, topic_model_corpus
 
 
 def create_topic_model_dictionary(list_responses):
-    """ Create a topic model dictionary from responses """
+    """ Create a topic model dictionary from the list of responses """
     # create the objects needed to prepare the dictionary
     tokenizer = RegexpTokenizer(r'\w+')
     en_stop = get_stop_words('en')
@@ -37,3 +38,16 @@ def create_topic_model_dictionary(list_responses):
         # turn the tokenized documents into a id <-> term dictionary
         topic_model_dictionary = corpora.Dictionary(texts_to_analyze)
     return topic_model_dictionary, texts_to_analyze
+
+
+def seed_display_topic_model_textually(seed_gensim_topic_model, seed_gensim_corpus, texts_to_analyze, num_topics):
+    """ Using only textual output provide a basic display of the topic model """
+    print(seed_gensim_topic_model)
+    print(seed_gensim_topic_model.print_topics(3))
+    print()
+    print("Analysis:")
+    for single_corpus, textual_document in zip(seed_gensim_corpus, texts_to_analyze):
+        print(single_corpus)
+        print(textual_document)
+        print(seed_gensim_topic_model[single_corpus])
+    print()
