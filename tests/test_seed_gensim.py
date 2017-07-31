1b37142ccb89198ca853e27ecd11b56ad07489fa
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-07-31 12:27:41 -0400

    Adding the test_seed_gensim_creates_model test case in the test_seed_gensim file.

--- tests/test_seed_gensim.py
+++ tests/test_seed_gensim.py
@@ -2,9 +2,19 @@
 
 import pytest
 
+import seed
 import seed_create
 import seed_download
 import seed_gensim
+
+VERIFIED = True
+NOT_VERIFIED = False
+
+
+@pytest.fixture
+def verifiable_seed_args_facts():
+    """Return arguments that are verifiable because only analyze_facts"""
+    return ['--analyze-facts', "--num-passes", "1", "--num-topics", "2"]
 
 
 def test_seed_gensim_creates_dictionary():
@@ -17,3 +27,15 @@ def test_seed_gensim_creates_dictionary():
     assert topic_model_dictionary is not None
     assert texts_to_analyze is not None
     assert len(topic_model_dictionary.keys()) != 0
+
+
+def test_seed_gensim_creates_model(verifiable_seed_args_facts):
+    """ Determine if seed_gensim can create a not-None model with LDA """
+    seed_arguments, seed_parser = seed.parse_seed_arguments(
+        verifiable_seed_args_facts)
+    seed_args_verified = seed.verify_seed_arguments(seed_arguments)
+    assert seed_args_verified == VERIFIED
+    seed_dictionary_list = seed_download.seed_load()
+    fact_response_list = seed_create.create_fact_answer_list(
+        seed_dictionary_list)
+    seed.perform_gensim_analysis(seed_arguments, fact_response_list)
