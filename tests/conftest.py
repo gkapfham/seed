da16c53d8a5d5456c1a3e7927297c690d60e8319
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-07-31 14:26:23 -0400

    Updating the conftest file so that it supports the running of slow test cases.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,13 @@
 import os
+# import pytest
 import sys
 
 # set the system path to contain the previous directory
 mypath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, mypath + '/../')
+
+
+# define command-line argument for including slow tests
+def pytest_addoption(parser):
+    parser.addoption(
+        "--runslow", action="store_true", help="Run the slow test cases")
