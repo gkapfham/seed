e770aa24cd92876d39f444e12304633485466c8f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-07-31 10:10:02 -0400

    Removing a not-needed import and reformatting the code in the test_seed_gensim file.

--- tests/test_seed_gensim.py
+++ tests/test_seed_gensim.py
@@ -2,7 +2,6 @@
 
 import pytest
 
-import seed
 import seed_create
 import seed_download
 import seed_gensim
@@ -11,8 +10,10 @@ import seed_gensim
 def test_seed_gensim_creates_dictionary():
     """ Determine if seed_gensim can create a not-None dictionary """
     seed_dictionary_list = seed_download.seed_load()
-    fact_response_list = seed_create.create_fact_answer_list(seed_dictionary_list)
-    topic_model_dictionary, texts_to_analyze = seed_gensim.create_topic_model_dictionary(fact_response_list)
+    fact_response_list = seed_create.create_fact_answer_list(
+        seed_dictionary_list)
+    topic_model_dictionary, texts_to_analyze = seed_gensim.create_topic_model_dictionary(
+        fact_response_list)
     assert topic_model_dictionary is not None
     assert texts_to_analyze is not None
     assert len(topic_model_dictionary.keys()) != 0
