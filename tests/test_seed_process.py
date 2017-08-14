2766a18983155bec94898960c7043e0734c84b3c
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-14 11:30:44 -0400

    Adding the skip feature to the tests in test_seed_process; also, adjusting spacing between functions in this file.

--- tests/test_seed_process.py
+++ tests/test_seed_process.py
@@ -8,14 +8,22 @@ import seed_download
 import seed_process
 
 
+SEED_SIMPLEFORM_TOKEN = "SEED_SIMPLEFORM_TOKEN"
+
+slow = pytest.mark.skipif(
+    not pytest.config.getoption("--runslow"),
+    reason="needs the --runslow option to run")
+
+
 @pytest.fixture
 def verifiable_seed_args():
     """Return arguments that are verifiable with the token specified through the OS"""
     # test case can only pass if the environment variable is set
-    simple_form_token = os.environ.get('SIMPLEFORM_TOKEN')
+    simple_form_token = os.environ.get(SEED_SIMPLEFORM_TOKEN)
     return ['--token', simple_form_token, '--download-json']
 
 
+@slow
 def test_dictionary_is_not_none(verifiable_seed_args):
     """Run seed with a specified token and it is verified"""
     seed_arguments, seed_parser = seed.parse_seed_arguments(verifiable_seed_args)
@@ -23,6 +31,7 @@ def test_dictionary_is_not_none(verifiable_seed_args):
     assert downloaded_json is not None
     seed_internal_dictionary = seed_process.seed_process_create_internal_dictionary(downloaded_json)
     assert seed_internal_dictionary is not None
+
 
 def test_dictionary_list_after_email_list_removal_is_smaller(verifiable_seed_args):
     """Make sure that the removal of the email subscriptions makes a smaller list"""
@@ -35,6 +44,7 @@ def test_dictionary_list_after_email_list_removal_is_smaller(verifiable_seed_arg
     assert length_internal_dictionary_list > 0
     seed_process.seed_process_remove_email_subscriptions(seed_internal_dictionary_list)
     assert len(seed_internal_dictionary_list) < length_internal_dictionary_list
+
 
 def test_dictionary_list_after_email_address_removal_is_same(verifiable_seed_args):
     """Make sure that the removal of the email address makes a same-sized list"""
