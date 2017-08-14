987d090526b94ee2d398d476939a820d59f8722b
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-14 11:44:10 -0400

    Adding a new testing parameter to the conftest; fixing comments here as well.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,11 @@ mypath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, mypath + '/../')
 
 
-# define command-line argument for including slow tests
+# define the viable command-line arguments when running tests
 def pytest_addoption(parser):
+    # define command-line argument for including slow tests
     parser.addoption(
         "--runslow", action="store_true", help="Run the slow test cases")
+    # define command-line argument for including tests that securely download files
+    parser.addoption(
+        "--rundownload", action="store_true", help="Run the downloading test cases")
