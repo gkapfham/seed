c127889f3f785d9b3956300ab6feeecdbb84c2d6
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-09-02 22:28:42 -0400

    Adding the sorting featuring :chart_with_upwards_trend: to the seed_display.

--- seed_display.py
+++ seed_display.py
@@ -3,13 +3,16 @@
 import json
 import random
 
-import seed_lookup
 import seed
+import seed_lookup
+import seed_process
 
 
 def seed_display_respondents(seed_dictionary_list):
     """ Displays all of the people who responded """
-    for current_seed_dictionary in seed_dictionary_list:
+    sorted_seed_dictionary_list = seed_process.seed_process_sort_dictionary_list(
+        seed_dictionary_list, seed_lookup.PERSON_NAME)
+    for current_seed_dictionary in sorted_seed_dictionary_list:
         current_name = current_seed_dictionary[seed_lookup.PERSON_NAME]
         print(seed.INDENT, current_name)
 
