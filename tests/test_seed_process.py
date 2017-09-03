868871949619a792d6f5fdea179b6eadc4c27a4a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-09-02 22:11:48 -0400

    Reformatting the code in the test_seed_process.

--- tests/test_seed_process.py
+++ tests/test_seed_process.py
@@ -7,7 +7,6 @@ import seed
 import seed_download
 import seed_process
 
-
 SEED_SIMPLEFORM_TOKEN = "SEED_SIMPLEFORM_TOKEN"
 
 download = pytest.mark.skipif(
@@ -26,36 +25,46 @@ def verifiable_seed_args():
 @download
 def test_dictionary_is_not_none(verifiable_seed_args):
     """Run seed with a specified token and it is verified"""
-    seed_arguments, seed_parser = seed.parse_seed_arguments(verifiable_seed_args)
+    seed_arguments, seed_parser = seed.parse_seed_arguments(
+        verifiable_seed_args)
     downloaded_json = seed_download.seed_download(seed_arguments.token)
     assert downloaded_json is not None
-    seed_internal_dictionary = seed_process.seed_process_create_internal_dictionary(downloaded_json)
+    seed_internal_dictionary = seed_process.seed_process_create_internal_dictionary(
+        downloaded_json)
     assert seed_internal_dictionary is not None
 
 
 @download
-def test_dictionary_list_after_email_list_removal_is_smaller(verifiable_seed_args):
+def test_dictionary_list_after_email_list_removal_is_smaller(
+        verifiable_seed_args):
     """Make sure that the removal of the email subscriptions makes a smaller list"""
-    seed_arguments, seed_parser = seed.parse_seed_arguments(verifiable_seed_args)
+    seed_arguments, seed_parser = seed.parse_seed_arguments(
+        verifiable_seed_args)
     downloaded_json = seed_download.seed_download(seed_arguments.token)
     assert downloaded_json is not None
-    seed_internal_dictionary_list = seed_process.seed_process_create_internal_dictionary(downloaded_json)
+    seed_internal_dictionary_list = seed_process.seed_process_create_internal_dictionary(
+        downloaded_json)
     assert seed_internal_dictionary_list is not None
     length_internal_dictionary_list = len(seed_internal_dictionary_list)
     assert length_internal_dictionary_list > 0
-    seed_process.seed_process_remove_email_subscriptions(seed_internal_dictionary_list)
+    seed_process.seed_process_remove_email_subscriptions(
+        seed_internal_dictionary_list)
     assert len(seed_internal_dictionary_list) < length_internal_dictionary_list
 
 
 @download
-def test_dictionary_list_after_email_address_removal_is_same(verifiable_seed_args):
+def test_dictionary_list_after_email_address_removal_is_same(
+        verifiable_seed_args):
     """Make sure that the removal of the email address makes a same-sized list"""
-    seed_arguments, seed_parser = seed.parse_seed_arguments(verifiable_seed_args)
+    seed_arguments, seed_parser = seed.parse_seed_arguments(
+        verifiable_seed_args)
     downloaded_json = seed_download.seed_download(seed_arguments.token)
     assert downloaded_json is not None
-    seed_internal_dictionary_list = seed_process.seed_process_create_internal_dictionary(downloaded_json)
+    seed_internal_dictionary_list = seed_process.seed_process_create_internal_dictionary(
+        downloaded_json)
     assert seed_internal_dictionary_list is not None
     length_internal_dictionary_list = len(seed_internal_dictionary_list)
     assert length_internal_dictionary_list > 0
     seed_process.seed_process_remove_emails(seed_internal_dictionary_list)
-    assert len(seed_internal_dictionary_list) == length_internal_dictionary_list
+    assert len(
+        seed_internal_dictionary_list) == length_internal_dictionary_list
